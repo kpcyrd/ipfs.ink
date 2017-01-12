27b5b645f84c9ee4468b21577922cc76a83b9769
kpcyrd <git@rxv.cc>
2017-01-12 18:45:34 +0100

    Finally fix race condition

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     var hash = location.pathname.split('/')[2];
 
     var render = function(text) {
